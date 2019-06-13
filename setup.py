5ce15bfb185b1b3e6584c3a958282aaeb9bd5825
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-06-13 23:34:12 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name = "bunnylog",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = ['testsimple>=0.1'],
+    url = "https://github.com/gkbrk/bunnylog-py",
+    author = "Gokberk Yaltirakli",
+    author_email = "opensource@gkbrk.com",
+    description = "BunnyCDN log parser",
+    keywords = "parser cdn log BunnyCDN",
+    project_urls = {
+        'Bug Tracker': 'https://github.com/gkbrk/bunnylog-py/issues',
+        'Source Code': 'https://github.com/gkbrk/bunnylog-py'
+    }
+)
