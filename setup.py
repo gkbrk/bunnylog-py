faa490f163f1a4ba11f497c60d928512d8e37f2f
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-06-13 23:37:58 +0100

    Shuffle files around

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "bunnylog",
-    version = "0.1.0",
+    version = "0.2.0",
     packages = find_packages(),
     install_requires = ['testsimple>=0.1'],
     url = "https://github.com/gkbrk/bunnylog-py",
